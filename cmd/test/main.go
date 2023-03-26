bbd500457562de69e78faaaff3d983812e009836
Qudecim <99prblmsaint1@gmail.com>
2023-03-27 00:09:58 +0300

    _

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -6,9 +6,14 @@ import (
 	"log"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/qudecim/password-manager-backend/internal/models/mysql"
 )
 
 func main() {
+	test2()
+}
+
+func test1() {
 	db, err := sql.Open("mysql",
 		"main:main@tcp(127.0.0.1:3306)/main")
 	if err != nil {
@@ -25,6 +30,7 @@ func main() {
 		log.Fatal(err)
 	}
 	defer rows.Close()
+
 	for rows.Next() {
 		err := rows.Scan(&id, &name)
 		if err != nil {
@@ -36,4 +42,16 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func test2() {
+	db, err := sql.Open("mysql",
+		"main:main@tcp(127.0.0.1:3306)/main")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	d := mysql.UserModel{DB: db}
+	fmt.Print(d.Get(1))
 }
